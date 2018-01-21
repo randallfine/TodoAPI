89b4898d822c666fa51bd2ca124ad9960562cadc
Randall Fine <randallfine@gmail.com>
2018-01-21 22:16:43 +0000

    set up mongo/mongoose on cloud9

--- todos_api/index.js
+++ todos_api/index.js
@@ -1,10 +1,11 @@
 const express = require("express"),
-      app = express();
+      app = express(),
+      port = process.env.PORT;
 
 app.get("/", function(req, res){
     res.send("HELLO FROM EXPRESS!!");
 });
 
-app.listen(process.env.PORT, function(){
+app.listen(port, function(){
     console.log("app is go at port: " + process.env.PORT);
 });
