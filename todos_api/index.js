0c55e33f9581bf22b121fa4d4c05ccef5b0299a0
Randall Fine <randallfine@gmail.com>
2018-01-26 02:50:52 +0000

    setup routes and helper files

--- todos_api/index.js
+++ todos_api/index.js
@@ -1,11 +1,19 @@
 const express = require("express"),
       app = express(),
-      port = process.env.PORT;
+      port = process.env.PORT || 3000,
+      bodyParser = require("body-parser");
+
+const todoRoutes = require("./routes/todos");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-    res.send("HELLO FROM EXPRESS!!");
+    res.send("HELLO FROM root route");
 });
 
+app.use("/api/todos", todoRoutes);
+
 app.listen(port, function(){
-    console.log("app is go at port: " + process.env.PORT);
+    console.log("app is go at port: " + port);
 });
--- todos_api/models/index.js
+++ todos_api/models/index.js
@@ -3,3 +3,6 @@ mongoose.set("debug", true);
 mongoose.connect("mongodb://localhost/todo-api");
 
 mongoose.Promise = Promise;
+
+module.exports.Todo = require("./todo")
+
